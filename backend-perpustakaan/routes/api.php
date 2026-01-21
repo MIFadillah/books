02efb36ee37a09ffdee5041311f90a39b2722209
Muhammadirfan08 <irfan020807@gmail.com>
2026-01-21 13:37:16 +0700

    feat(route): add book api resource routes

--- backend-perpustakaan/routes/api.php
+++ backend-perpustakaan/routes/api.php
@@ -3,6 +3,8 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\BookController;
+
 
 Route::post('/login', [AuthController::class, 'login']);
 Route::apiResource('/books', BookController::class);
